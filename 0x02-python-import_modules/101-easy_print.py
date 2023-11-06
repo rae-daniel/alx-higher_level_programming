3a291bd9934e8c16df0aec682af9c5cce2d5f47f
rae-daniel <ritadaniel740@gmail.com>
2023-11-06 15:59:22 -0800

    0x02. Python - import & modules

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-import python_is_cool
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print #pythoniscool, followed by a new line,
+# in the standard output.
+#
+# (C) 2023 Rita Udeh, Abuja, Nigeria
+# email ritadniel740@gmail.com
+# -----------------------------------------------------------
+
+exec('import subprocess; subprocess.call(["echo", "#pythoniscool"])')
