475c7ea74174894a0a989af10179a65b21b35ebe
rae-daniel <ritadaniel740@gmail.com>
2023-11-06 16:09:48 -0800

    0x02. Python - import & modules

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,11 +1,2 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to print #pythoniscool, followed by a new line,
-# in the standard output.
-#
-# (C) 2023 Rita Udeh, Abuja, Nigeria
-# email ritadniel740@gmail.com
-# -----------------------------------------------------------
-
-exec('import subprocess; subprocess.call(["echo", "#pythoniscool"])')
+__import__("sys").stdout.write("#pythoniscool\n")
--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,9 +2,9 @@
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
-        sum = add(a, b)
+        c = add(a, b)
         for i in range(4, 6):
-            sum = add(sum, i)
-            return sum
-        else:
-            return sub(a, b)
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
