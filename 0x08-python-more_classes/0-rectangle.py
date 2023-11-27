6c37e15835eb8707b9e042c0551263efe1c801b2
rae-daniel <ritadaniel740@gmail.com>
2023-11-27 13:50:18 -0800

    0x08-python-more_classes project

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines the empty class rectangle
+"""
+
+class Rectangle:
+    """ class rectangle"""
+    pass
---
+++ 0x08-python-more_classes/9-main.py
@@ -0,0 +1,7 @@
+guillaume@ubuntu:~/0x08$ cat 9-main.py
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+my_square = Rectangle.square(5)
+print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
+print(my_square)
